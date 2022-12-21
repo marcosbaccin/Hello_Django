7b78b36498a57f8b78a0a7e83a77fdcec34d196b
Marcos Baccin <marcos.baccin@estudante.ifgoiano.edu.br>
2022-12-21 14:26:21 -0300

    Exercício para adicionar rotas a nossa aplicação

--- core/views.py
+++ core/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
+
+def soma(request, valor1, valor2):
+    soma = valor1 + valor2
+    return HttpResponse('{} + {} = {}'.format(valor1, valor2, soma))
+
+def subtracao(request, valor1, valor2):
+    subtracao = valor1 - valor2
+    return HttpResponse('{} - {} = {}'.format(valor1, valor2, subtracao))
+
+def multiplicacao(request, valor1, valor2):
+    multiplicacao = valor1 * valor2
+    return HttpResponse('{} * {} = {}'.format(valor1, valor2, multiplicacao))
+
+def divisao(request, valor1, valor2):
+    divisao = valor1 / valor2
+    return HttpResponse('{} / {} = {}'.format(valor1, valor2, divisao))
